8079fadca421f87219a9d6246f7be0e81c9064e9
Silvina <msilvinahippler@gmail.com>
2025-05-20 14:39:45 -0300

    punto_9.cpp

--- README.md
+++
@@ -1,4 +0,0 @@
-//Ejercicio_3 de info.General
-//Mejorar el ejemplo del “juego_de_ordenar_las_letras” de la clase sobre String y Arreglos agregándole un sistema de puntaje. Hacer que el puntaje varíe según la cantidad de letras de la palabra que hay que adivinar. También hacer que se vayan restando puntos cada vez que se utilice la ayuda para tener una pista.
-
-#
---
+++ punto_9.cpp
@@ -0,0 +1,44 @@
+//Ejercicio_3 de info.General
+//Mejorar el ejemplo del “juego_de_ordenar_las_letras” de la clase sobre String y Arreglos agregándole un sistema de puntaje. Hacer que el puntaje varíe según la cantidad de letras de la palabra que hay que adivinar. También hacer que se vayan restando puntos cada vez que se utilice la ayuda para tener una pista.
+
+#include <iostream>
+
+#include <string>
+
+#include <ramdon>
+
+#include <time()>
+
+
+
+int cantidadp [] = ;
+
+int [];
+
+int palabras[] = {"mate", "informatica", "compu"};
+
+int ?[] = {"sin el no pasas el cuatri", "la materia que mas te cuesta en multi", "donde te pasas la mayor parte del tiempo"};
+
+const palabra[] ={"mate", "informatica", "compu"};
+
+int puntajeI = 3;
+
+int puntaje? = 1;
+
+int puntajeg = 1;
+
+std;
+
+int main () {
+
+cout <<"Ordena la palabra y presiona ´?´ si queres ayuda" << end;  
+
+  
+
+  
+
+  
+
+  return 0;
+
+}
